0312f4eb64a00036cfcd9d5aecf6f051615b9af4
Alansan <alaansannchezz@gmail.com>
2022-10-23 15:02:12 -0300

    feat: Se añadio validaciones al Register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
@@ -15,8 +16,10 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
+        $request->validated();
+
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
