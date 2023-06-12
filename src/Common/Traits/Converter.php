b30f379eaa4113decff30b414897f98c87731b81
Alan Sanchez <alaansannchezz@gmail.com>
2023-06-12 19:08:05 -0300

    feat: Se añadio el metodo randomCode

--- src/Common/Traits/Converter.php
+++ src/Common/Traits/Converter.php
@@ -2,11 +2,18 @@
 
 namespace Src\Common\Traits;
 
+use Illuminate\Support\Str;
+
 trait Converter
 {
     public function lower($string): string
     {
         return strtolower($string);
+    }
+
+    public function randomCode(int $lenght): string
+    {
+        return Str::upper(Str::random($lenght));
     }
 
     public function capitalized($string): string
