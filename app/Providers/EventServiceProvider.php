42148e482ff8e0fdc5f7106739d5501a2987c172
Alan Sanchez <alaansannchezz@gmail.com>
2023-06-12 19:10:53 -0300

    fix: se comento una linea que genera un error por la falta de la entidad Posts

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -29,7 +29,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        PostModel::observe(PostObserver::class);
+        //PostModel::observe(PostObserver::class);
     }
 
     /**
